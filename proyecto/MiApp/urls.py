45424e2f900eea28f4f263669fbfc973206e3b70
NicolasAmato <nicolasamato@outlook.com>
2022-11-16 19:55:55 -0300

    TODO OK PARA entregar

--- proyecto/MiApp/urls.py
+++ proyecto/MiApp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from MiApp.views import index, nuevo_deporte, nuevo_profesor, nuevo_alumno, buscar_CLUB, buscar_profesor 
+from MiApp.views import index, nuevo_deporte, nuevo_profesor, nuevo_alumno, buscar_DEPORTE, buscar_profesor 
 from .forms import club_deportivoForm, profesorForm, alumnoForm
 
 
@@ -8,7 +8,7 @@ urlpatterns = [
     path('nuevo_deporte/', nuevo_deporte, name='Nuevo Deporte'),
     path('nuevo_profesor/', nuevo_profesor, name='Nuevo Profesor'),
     path('nuevo_alumno/', nuevo_alumno, name='Nuevo Alumno'),
-    path('buscar_CLUB/', buscar_CLUB, name='buscar CLUB'),
+    path('buscar_DEPORTE/', buscar_DEPORTE, name='buscar DEPORTE'),
     path('buscar_profesor/', buscar_profesor, name='buscar PROFESOR'),
     path('club_deportivoForm/', club_deportivoForm),
     path('profesorForm/', profesorForm),
